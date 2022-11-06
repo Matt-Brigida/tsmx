29aee808db51300923c4d884551f4ef199860672
Matt Brigida <mdbrigida@gmail.com>
2022-11-06 11:52:37 -0500

    start sim vasicek

---
+++ src/tsmx/vasicek/simulate.py
@@ -0,0 +1,14 @@
+import jax.numpy as jnp
+
+def sim(alpha, beta, sigma, steps):
+    """
+    Simulate the Vasicek short rate model:
+    dr = alpha(beta - r)dt + sigma dW
+    """
+    i = 0
+    while (i < steps):
+        
+
+
+
+        i =+ 1
